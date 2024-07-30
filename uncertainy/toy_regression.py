3de2ef8a18eaa0cb736b645e27319e83ab31f80d
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2024-07-30 15:30:48 +0200

    train simple regression model

--- uncertainy/toy_regression.py
+++ uncertainy/toy_regression.py
@@ -1,41 +1,42 @@
 import math
 from typing import Tuple
 
-import numpy as np
 import torch
-from scipy import stats
 from torch.utils.data import DataLoader, TensorDataset
 
 
-def create_data_loader(x, y, batch_size, num_workers):
-    dataset = TensorDataset(torch.tensor(x).float(), torch.tensor(y).float())
+def create_data_loader(x, y, batch_size, num_workers, shuffle: bool):
+    dataset = TensorDataset(x, y)
     return DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True,
                       num_workers=num_workers, pin_memory=True, persistent_workers=True)
 
 
+def sample_dataset(start, end, n):
+    x = torch.linspace(start, end, n)
+    sample_mean = torch.sin(x / 2)
+    sample_var = ((abs(start) + abs(end)) / 2 - torch.abs(x)) / 16
+    y = torch.normal(sample_mean, sample_var)
+
+    # expand dims
+    x = x.unsqueeze(1)
+    y = y.unsqueeze(1)
+    return x, y
+
+
 def get_toy_regression_data_loaders(batch_size: int, num_workers: int, num_points: int) -> Tuple[
     DataLoader, DataLoader, DataLoader]:
-    x = np.linspace(-10, 10, num_points)
-    sample_mean = [math.sin(i / 2) for i in x]
-    sample_var = [((abs(-10) + abs(10)) / 2 - abs(i)) / 16 for i in x]
-    y = stats.norm(sample_mean, sample_var).rvs()
-
-    # Shuffle the data
-    indices = np.random.permutation(num_points)
-    x, y = x[indices], y[indices]
-
     # Split the data into train, validation, and test sets
     train_size = int(0.7 * num_points)
     val_size = int(0.15 * num_points)
     test_size = num_points - train_size - val_size
 
-    x_train, y_train = x[:train_size], y[:train_size]
-    x_val, y_val = x[train_size:train_size + val_size], y[train_size:train_size + val_size]
-    x_test, y_test = x[train_size + val_size:], y[train_size + val_size:]
+    x_train, y_train = sample_dataset(-7, 7, train_size)
+    x_val, y_val = sample_dataset(-7, 7, val_size)
+    x_test, y_test = sample_dataset(-10, 10, test_size)
 
-    train_loader = create_data_loader(x_train, y_train, batch_size, num_workers)
-    val_loader = create_data_loader(x_val, y_val, batch_size, num_workers)
-    test_loader = create_data_loader(x_test, y_test, batch_size, num_workers)
+    train_loader = create_data_loader(x_train, y_train, batch_size, num_workers, shuffle=True)
+    val_loader = create_data_loader(x_val, y_val, batch_size, num_workers, shuffle=False)
+    test_loader = create_data_loader(x_test, y_test, batch_size, num_workers, shuffle=False)
 
     # print shapes
     print(f"Train loader shape: {len(train_loader.dataset)}")
@@ -43,3 +44,10 @@ def get_toy_regression_data_loaders(batch_size: int, num_workers: int, num_point
     print(f"Test loader shape: {len(test_loader.dataset)}")
 
     return train_loader, val_loader, test_loader
+
+
+if __name__ == '__main__':
+    loader, _, _ = get_toy_regression_data_loaders(32, 4, 100)
+
+    x, y = next(iter(loader))
+    print(x.shape, y.shape)
